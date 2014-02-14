1ce533ddaa9edc03072ff63a9b793025742e8b10
hramovecchi <hramovecchi@gmail.com>
2014-02-14 16:18:45 -0300

    adding the joymeter-webapi project

---
+++ dev/joymeter-webapi/src/main/java/com/joymeter/MyResource.java
@@ -0,0 +1,34 @@
+package com.joymeter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    @GET
+    @Path("/.json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getThat() {
+    	String aux = "\"response\""+":" + "\"Got it!\"";
+        return Response.ok("{"+aux+"}").build();
+    }
+}
