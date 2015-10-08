998389fbe9befb2b445ba31d311a60ce63c2a644
hramovecchi <hramovecchi@gmail.com>
2015-10-08 12:31:26 -0300

    adding menu on history activity

--- dev/joymeter-android-client/AndroidClient/app/src/main/java/com/joymeter/androidclient/HistoryActivity.java
+++ dev/joymeter-android-client/AndroidClient/app/src/main/java/com/joymeter/androidclient/HistoryActivity.java
@@ -1,7 +1,10 @@
 package com.joymeter.androidclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by hramovecchi on 15/09/2015.
@@ -12,5 +15,29 @@ public class HistoryActivity extends FragmentActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_history, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            Intent i = new Intent(getApplicationContext(), SingleActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
