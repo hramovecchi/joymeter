0190b9c415b95aa6d7bb74d5650f8634423ddeec
hramovecchi <hernan.ramovecchi@am.sony.com>
2015-11-12 13:55:03 -0300

    Formating the code

--- dev/joymeter-android-client/AndroidClient/app/src/main/java/com/joymeter/androidclient/dialog/TimePickerFragment.java
+++ dev/joymeter-android-client/AndroidClient/app/src/main/java/com/joymeter/androidclient/dialog/TimePickerFragment.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
