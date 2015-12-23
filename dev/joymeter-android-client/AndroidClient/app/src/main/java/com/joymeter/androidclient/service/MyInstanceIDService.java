da16ed0885927d89ea71f13172d7f8b5c0a6e8f2
hramovecchi <hernan.ramovecchi@am.sony.com>
2015-12-23 17:17:53 -0300

    gcm notifications

--- dev/joymeter-android-client/AndroidClient/app/src/main/java/com/joymeter/androidclient/service/MyInstanceIDService.java
+++ dev/joymeter-android-client/AndroidClient/app/src/main/java/com/joymeter/androidclient/service/MyInstanceIDService.java
@@ -3,5 +3,5 @@ package com.joymeter.androidclient.service;
 /**
  * Created by hernan.ramovecchi on 02/12/2015.
  */
-public class MyInstanceIDListenerService {
+public class MyInstanceIDService {
 }
